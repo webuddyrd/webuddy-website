6ea7e0daf80dbaa06d80bc0addba5aa6b079f51c
Webuddy <webuddyrd@gmail.com>
2025-10-30 17:54:32 -0400

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/webuddyrd/webuddy-website',
+  base: '/webuddy-website',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
