40d39d2bff370d41536dfc52f659d2ba78659195
Webuddy <webuddyrd@gmail.com>
2026-05-28 14:59:11 -0400

    feat: change porftolio card from company logo to project result.

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,10 +1,3 @@
-import OrkiBS from '../images/Orki-logo.png';
-import Germsout from '../images/Germsout-logo.png';
-import Cruva from '../images/Cruva-logo.png';
-import Spring from '../images/Spring-logo.png';
-import Blue from '../images/Blue-logo.png';
-import Proyecto455 from '../images/Proyecto455-logo.png';
-
 export interface PortfolioItem {
   title: string;
   description_en: string;
@@ -19,7 +12,7 @@ export const portfolio: PortfolioItem[] = [
     title: "Spring Tours",
     description_en: "Vibrant tourism site with responsive tour listings and booking features.",
     description_es: "Sitio web turístico dinámico con listados de tours interactivos y funciones de reserva.",
-    image: Spring,
+    image: "projects/spring-tours.webp",
     url: "https://spring-tours.com/",
     category: "Web"
   },
@@ -27,7 +20,7 @@ export const portfolio: PortfolioItem[] = [
     title: "Blue Digital Agency",
     description_en: "Digital agency website showcasing services and client portfolio with a modern, animated design.",
     description_es: "Sitio web de agencia digital que muestra servicios y portafolio de clientes con un diseño animado y moderno.",
-    image: Blue,
+    image: "projects/blue.webp",
     url: "https://www.blueadigital.com/",
     category: "Web"
   },
@@ -35,7 +28,7 @@ export const portfolio: PortfolioItem[] = [
     title: "Proyecto 455",
     description_en: "Digital agency website showcasing services and client portfolio with a modern, animated design.",
     description_es: "Sitio web de agencia digital que muestra servicios y portafolio de clientes con un diseño animado y moderno.",
-    image: Proyecto455,
+    image: "projects/proyecto-455.webp",
     url: "https://www.proyecto455.com/",
     category: "Web"
   },
@@ -43,7 +36,7 @@ export const portfolio: PortfolioItem[] = [
     title: "Cruva Construcciones",
     description_en: "Professional corporate website showcasing construction services and portfolio.",
     description_es: "Sitio web corporativo profesional que muestra servicios de construcción y portafolio.",
-    image: Cruva,
+    image: "projects/cruva.webp",
     url: "https://cruvaconstrucciones.com/",
     category: "Web"
   },
@@ -51,16 +44,17 @@ export const portfolio: PortfolioItem[] = [
     title: "Germsout Dominicana",
     description_en: "Online store for hygiene and cleaning products with smooth shopping experience.",
     description_es: "Tienda online de productos de higiene y limpieza con una experiencia de compra fluida.",
-    image: Germsout,
+    image: "projects/germsout.webp",
     url: "https://germsout.store/",
     category: "E-Commerce"
-  },
-  {
-    title: "Orki Beauty Solutions",
-    description_en: "Elegant e-commerce site for beauty supplies with a clean, modern design.",
-    description_es: "Elegante sitio de comercio electrónico de productos de belleza con un diseño limpio y moderno.",
-    image: OrkiBS,
-    url: "http://orkibeautysolutions.com/",
-    category: "E-Commerce"
   }
+  // ,
+  // {
+  //   title: "Orki Beauty Solutions",
+  //   description_en: "Elegant e-commerce site for beauty supplies with a clean, modern design.",
+  //   description_es: "Elegante sitio de comercio electrónico de productos de belleza con un diseño limpio y moderno.",
+  //   image: OrkiBS,
+  //   url: "http://orkibeautysolutions.com/",
+  //   category: "E-Commerce"
+  // }
 ];
